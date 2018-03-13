157963ccb82d35d79ab95ad338a0353389a4b7ae
James Cole <jamescole@gmail.com>
2018-03-13 23:58:12 +1000

    Refactor into separate functions.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -5,15 +5,27 @@ Broken program to determine score status
 score must be between 0 and 100 inclusive; 90 or more is excellent; 50 or more is a pass; below 50 is bad
 """
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
+
+def main():
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
         print("Invalid score")
-    elif score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Pass")
     else:
-        print("Bad")
+        result = get_result_for_score(score)
+        print(result)
+
+
+def get_result_for_score(score):
+    """
+    Assumes score is >= 0 and <= 100
+    """
+    if score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Pass"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
