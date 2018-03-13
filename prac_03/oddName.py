10a9001025da28a8c474a681cefa10299ae2112c
James Cole <jamescole@gmail.com>
2018-03-13 23:04:09 +1000

    Refactor into multiple functions. Generalise handling of 'step' used to sample characters from name

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -4,8 +4,29 @@ Asks the user for their name, checks it isn't blank, then prints every second le
 James Cole
 """
 
-name = input("Enter your name: ")
-while name == "":
-    print("Error, empty name string")
+
+def main():
+    name = get_name()
+    step = 2
+    print(get_every_nth_letter(name, step))
+
+
+def get_name():
+    """
+    get user's name, repeatedly asking for input until a non-empty string is entered
+    """
     name = input("Enter your name: ")
-print(name[1::2])
+    while name == "":
+        print("Error, empty name string")
+        name = input("Enter your name: ")
+    return name
+
+
+def get_every_nth_letter(string, step):
+    """
+    return a string containing every Nth letter (where N is indicated by 'step') of string
+    """
+    return string[step - 1::step]
+
+
+main()
