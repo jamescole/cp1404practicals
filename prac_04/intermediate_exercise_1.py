305b5f33fe5ad18e0392d2a49912c7debfaaa4e3
James Cole <jamescole@gmail.com>
2018-03-19 17:00:50 +1000

    Enable user to enter any number of inputs

--- prac_04/intermediate_exercise_1.py
+++ prac_04/intermediate_exercise_1.py
@@ -1,15 +1,16 @@
-QUANTITY_OF_INPUT_NUMBERS = 5
-
-
 def main():
     numbers = []
-    for index in range(QUANTITY_OF_INPUT_NUMBERS):
-        numbers.append(int(input("Number: ")))
+    print("Enter negative value to stop entering inputs.")
+    number = int(input("Number {}: ".format(len(numbers) + 1)))
+    while number >= 0:
+        numbers.append(number)
+        number = int(input("Number {}: ".format(len(numbers) + 1)))
 
     print("The first number is {}".format(numbers[0]))
     print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
     print("The largest number is {}".format(max(numbers)))
     print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
 
 main()
