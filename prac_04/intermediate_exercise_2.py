00769f7f25cb9678032340a82c3f7345e18eaa85
James Cole <jamescole@gmail.com>
2018-03-19 15:12:54 +1000

    Add solution to prac 4 Intermediate Exercises 2.

---
+++ prac_04/intermediate_exercise_2.py
@@ -0,0 +1,10 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username: ")
+
+# assuming user names are case sensitive
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
