09fb7951ea97fa6249a12574fbd1d31467d5d258
James Cole <jamescole@gmail.com>
2018-03-19 17:41:24 +1000

    Add solution to prac 4 Practice and Extension exercise 4

---
+++ prac_04/practice_and_extension_4.py
@@ -0,0 +1,27 @@
+def main():
+    list1 = [1, 2, 3, 1, 1, 1, 1]
+    list2 = [2, 7, -14, 4, 11]
+    memberwise_addition_list = memberwise_addition(list1, list2)
+    print(memberwise_addition_list)
+
+
+def memberwise_addition(list1, list2):
+    """
+    Returns the list that contains the sum of elements that are in the same index in the two lists.
+    Both input lists should have same length; function assumes this is the case.
+    """
+    longest_list, shortest_list = [], []
+    if len(list1) > len(list2):
+        longest_list = list1
+        shortest_list = list2
+    else:
+        longest_list = list2
+        shortest_list = list1
+    memberwise_addition_list = [shortest_list_number + longest_list[i] for i, shortest_list_number in
+                                enumerate(shortest_list)]
+    if len(list1) != len(list2):
+        memberwise_addition_list += longest_list[len(shortest_list):]
+    return memberwise_addition_list
+
+
+main()
