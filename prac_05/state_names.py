6fc493a48f4eec44ef2ea020d2804478d9cb2923
James Cole <jamescole@gmail.com>
2018-03-27 16:04:23 +1000

    Add print_state_abbreviations_and_names()

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,9 +4,18 @@ State names in a dictionary
 File needs reformatting
 """
 
+
+def print_state_abbreviations_and_names():
+    for state_abbreviation in STATE_NAMES:
+        print("{:4} is {}".format(state_abbreviation, STATE_NAMES[state_abbreviation]))
+
+
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
+
+print_state_abbreviations_and_names()
+print()
 
 state = input("Enter short state: ").upper()
 while state != "":
