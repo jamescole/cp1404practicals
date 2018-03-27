baaa8b956f79758175d1db14d439520f402b51a7
James Cole <jamescole@gmail.com>
2018-03-27 21:51:26 +1000

    Refactor to store tariffs in dictionary.

--- prac_05/electricity_bill_estimator.py
+++ prac_05/electricity_bill_estimator.py
@@ -1,23 +1,18 @@
-TARIFF_11 = 0.244518
-TARIFF_31 = 0.136928
+TARIFFS = {"11": 0.244518, "31": 0.136928, "35": 0.35, "70": 0.70, "99": 0.99}
 
 print("Electricity bill estimator")
 
 price_per_kWh_in_cents = -1
 valid_tariff_type_entered = False
 while not valid_tariff_type_entered:
-    tariff_type = input("Which tarrif? 11 or 31: ")
-    if tariff_type == "11":
-        price_per_kWh_in_cents = TARIFF_11
-        valid_tariff_type_entered = True
-    elif tariff_type == "31":
-        price_per_kWh_in_cents = TARIFF_31
+    prompt = "Which tariff? " + " or ".join(TARIFFS.keys()) + ": "
+    tariff_type = input(prompt)
+    if tariff_type in TARIFFS.keys():
+        price_per_kWh_in_cents = TARIFFS[tariff_type]
         valid_tariff_type_entered = True
     else:
         print("invalid tariff type")
 
-
-# price_per_kWh_in_cents = int(raw_input("Enter cents per kWh: "))
 daily_use_in_kWh = int(input("Enter daily use in kWh: "))
 number_days_in_billing_period = int(input("Enter number of billing days: "))
 
