77b007de1486108c8016e86ce844f2500680ca95
James Cole <jamescole@gmail.com>
2018-04-09 17:44:30 +1000

    Add name attribute

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -3,7 +3,8 @@ DYNAMIC = "DYNAMIC"
 
 class ProgrammingLanguage:
 
-    def __init__(self, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
