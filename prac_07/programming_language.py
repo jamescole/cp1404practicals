71888c1215bd7f7bb3b27ad504d421f346beaf3f
James Cole <jamescole@gmail.com>
2018-04-09 17:54:18 +1000

    Add __str__ method

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -9,5 +9,9 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        format_string = "{}, {} Typing, Reflection={}, First appeared in {}"
+        return format_string.format(self.name, self.typing, self.reflection, self.year)
+
     def is_dynamic(self):
         return self.typing.upper() == DYNAMIC
